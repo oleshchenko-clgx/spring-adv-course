53e6cdedffcabbccaa655d63e19caaa4dc3bbbdf
Oleksii Leshchenko <oleshchenko@corelogic.com>
2019-02-25 20:06:23 +0200

    Trying to configure Spring MVC application. It still does not work.

--- src/main/java/beans/controllers/BookingController.java
+++ src/main/java/beans/controllers/BookingController.java
@@ -2,16 +2,24 @@ package beans.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 @Controller
 public class BookingController {
 
-    @RequestMapping("/welcome")
+    @RequestMapping(value = "/booking", method = RequestMethod.GET)
     public ModelAndView helloWorld() {
 
         String message = "<br><div style='text-align:center;'>"
                 + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
         return new ModelAndView("welcome", "message", message);
+    }
+
+    @RequestMapping(value = "/")
+    public String root(){
+
+        return "welcome";
     }
 }
